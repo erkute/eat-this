8c74177269c5e914d1463505f8004ddae4720ad2
erkute <135225355+erkute@users.noreply.github.com>
2026-06-01 11:18:41 +0200

    feat(schema): add restaurant.featuredOnDate + homeWeek doc-type
    Part of #49

--- studio/schemaTypes/index.js
+++ studio/schemaTypes/index.js
@@ -5,5 +5,6 @@ import staticPage from './staticPage'
 import bezirk from './bezirk'
 import category from './category'
 import waitlistSignup from './waitlistSignup'
+import homeWeek from './homeWeek'
 
-export const schemaTypes = [newsArticle, mustEat, restaurant, bezirk, category, staticPage, waitlistSignup]
+export const schemaTypes = [newsArticle, mustEat, restaurant, bezirk, category, staticPage, waitlistSignup, homeWeek]
